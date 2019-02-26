54c9bf0505f9949af7b6a2422c1b1ba9afa04c71
Arthuro <artgrohe@googlemail.com>
2019-02-26 16:43:53 +0100

    Greet new user and inform others

--- server/server.js
+++ server/server.js
@@ -15,18 +15,37 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit from Admin text Welcome to chat app
+    socket.emit('newMessage', {
+        from: 'Server Bot',
+        text: 'Welcome to the chat app :-)',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.broadcast.emit from Admin text new User joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Server Bot',
+        text: 'New user joined!',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 
     socket.on('createMessage', (message) => {
         console.log('got message:', message);
-        // io.emit is a broad cast !!!
+        // io.emit is a global cast !!!
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 });
 
